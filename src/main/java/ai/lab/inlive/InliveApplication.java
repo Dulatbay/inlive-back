7598bbc7a4545123f6d4e8bf806c9895eb309470
akhan <akhan.dulatbay@gmail.com>
2025-09-29 16:55:12 +0500

    Initialize Spring Boot project with Gradle build system, application properties, core dependencies, and basic application setup.

---
+++ src/main/java/ai/lab/inlive/InliveApplication.java
@@ -0,0 +1,13 @@
+package ai.lab.inlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InliveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/lab/inlive/InliveApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.lab.inlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
