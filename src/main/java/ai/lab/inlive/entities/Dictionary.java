89fbc0110bdd58783da16904fba8f6c985131719
akhan <akhan.dulatbay@gmail.com>
2025-10-11 04:15:32 +0500

    fixes

--- src/main/java/ai/lab/inlive/entities/Dictionary.java
+++ src/main/java/ai/lab/inlive/entities/Dictionary.java
@@ -12,7 +12,10 @@ import lombok.EqualsAndHashCode;
 @Table(name = "dictionaries")
 public class Dictionary extends AbstractEntity<Long> {
     @Column(nullable = false)
-    private String key;
+    private String key; // todo: enum
+
+    // todo: transform all fields to snake case
+    // todo: create paginated response(base class for extends) for some queries (KZH, WONDER)
 
     @Column(nullable = false)
     private String value;
